d60a8db7b68a3fb3e2ca509df28bd515f3b6f86f
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2019-02-17 21:15:56 +0900

    20190217

---
+++ 1st/2.js
@@ -0,0 +1,35 @@
+const log = console.log;
+
+const a = 10;
+const add10 = a => a + 10;
+const r = add10(a);
+
+const add5 = a => a + 5;
+log(add5);
+log(add5(5));
+
+const f1 = () => () => 1;
+log(f1());
+
+const f2 = f1();
+log(f2);
+log(f2());
+
+const apply1 = f => f(1);
+const add2= a => a + 2;
+log(apply1(add2));
+log(apply1( a => a -1));
+
+const times = (f , n) => {
+    let i = -1;
+    while ( ++i < n) f(i);
+}
+times(log, 3);
+
+const addMaker = a => b => a +b;
+const add10 = addMaker(10);
+log(add10(5));
+log(add10(10));
+
+times( a=> log( a + 10), 3);
+
---
+++ 1st/3.js
@@ -0,0 +1,20 @@
+const log = console.log;
+
+const list = [1, 2, 3];
+for (let i = 0; i < list.length; i++){
+    log(list[i]);
+}
+
+const str = 'abc';
+for (let i = 0; i < str.length; i++) {
+    log(list[i]);
+
+}
+
+for ( const a of list ) {
+    log(a);
+}
+
+for (const a of str) {
+    log(a);
+}
