a01176b018f1d929b0852bc2f04bbbf0e303c7c6
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2019-02-22 02:45:56 +0900

    20190222_2

--- 1st/5.js
+++ 1st/5.js
@@ -103,3 +103,79 @@ m.set('b', 20);
 const it = m[Symbol.iterator]();
 log(new Map(map(([k,a]) =>[k, a * 2], m ))); // 키와 값을 뒤바꿀 수 있다. 이거 정말 좋다.
 
+// let under20000 = [];
+// for (const p of products) {
+//     if(p.price < 20000) under20000.push(p);
+// }
+//
+// log(...under20000);
+//
+// let over20000 = [];
+// for (const p of products) {
+//     if(p.price >= 20000) over20000.push(p);
+// }
+
+// log(...over20000);
+
+const filter = (f, iter) => {
+    let res = [];
+    for (const a of iter) {
+        if (f(a)) res.push(a);
+    }
+
+    return res;
+}
+
+log(...filter(p => p.price < 20000, products));
+log(...filter(p => p.price >= 20000, products));
+
+log(filter(n => n % 2, [1,2,3,4] ));
+
+log(filter(n => n % 2, function *() {
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+}()));
+
+const nums = [1,2,3,4,5];
+
+let total = 0;
+for ( const n of nums ) {
+    total= total + n;
+}
+log(total);
+
+const reduce = (f, acc, iter) => {
+    if (!iter) {
+        iter = acc[Symbol.iterator]();
+        acc = iter.next().value;
+    }
+    for ( const a of iter) {
+        acc = f(acc, a);
+    }
+    return acc;
+};
+
+const add = (a ,b) => a +b;
+log(reduce(add, 0, [1,2,3,4,5]));
+log(add(add(add(add(add(0,1),2),3),4),5));
+
+
+log(reduce(add, [1,2,3,4,5]));
+
+log(reduce(
+    (total_price, product) => total_price + product.price,0,products ));
+
+const add1 = (a ,b) => a + b;
+
+log(reduce(// 하나의 값으로 만들 때
+    add1,
+    map(p=>p.price,// 특정 숫자로 평가될 때
+        filter(p=> p.price < 20000,products)))); //특정 조건으로 평가될 때
+
+log(reduce(
+    add1,
+    filter(n => n >= 20000,
+        map(p=> p.price,products))));
