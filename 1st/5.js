9a95faaf68b3bcfd59eb9cc3325ce2c5935339ab
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2019-02-22 01:03:59 +0900

    20190222

--- 1st/5.js
+++ 1st/5.js
@@ -53,3 +53,53 @@ const [a, b, ...rest] = odds(10);
 log(a);
 log(b);
 log(rest);
+
+const products = [
+    { name : '반팔티', price:15000},
+    { name : '긴팔티', price:20000},
+    { name : '핸드폰케이스', price:15000},
+    { name : '후드티', price:30000},
+    { name : '바지', price:25000}
+];
+
+const map = (f,iter) => {
+
+    let res = [];
+    for (const a of iter ) {
+        res.push(f(a));
+    }
+    return res;
+};
+
+let prices = [];
+for (const p of products ) {
+    prices.push(p.price);
+}
+
+
+log(map(p=> p.name, products));
+log(map(p=> p.price, products));
+
+log([1,2,3].map(a=>a + 1));
+log(map(el => el.nodeName, document.querySelectorAll('*')));
+
+// const it = document.querySelectorAll('*')[Symbol.iterator]();
+// log(it.next());
+// log(it.next());
+// log(it.next());
+// log(it.next());
+// log(it.next());
+
+function  *gen() {
+    yield 2;
+   if (false)  yield 3;
+    yield 4;
+}
+log(map(a=> a * a, gen()));
+
+let m = new Map();
+m.set('a', 10);
+m.set('b', 20);
+const it = m[Symbol.iterator]();
+log(new Map(map(([k,a]) =>[k, a * 2], m ))); // 키와 값을 뒤바꿀 수 있다. 이거 정말 좋다.
+
