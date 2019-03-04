e14fa71f743712bf3f7c2bc2037ac3bd6ea34233
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2019-03-05 07:21:02 +0900

    20190305_2

--- 1st/7.js
+++ 1st/7.js
@@ -2,6 +2,8 @@
 const log = console.log;
 const curry = f => (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
+const add = (a ,b) => a +b;
+
 const reduce = curry((f, acc, iter) => {
     if (!iter) {
         iter = acc[Symbol.iterator]();
@@ -29,7 +31,7 @@ const filter = curry((f, iter) => {
     return res;
 });
 
-const add = (a,b) => a + b;
+
 
 const L = {};
 
@@ -46,7 +48,14 @@ L.range = function *(l) {
 //
 // log(L.range(2));
 
-
+const range = l => {
+    let i = -1;
+    let res = [];
+    while (++i < l) {
+        res.push(i);
+    }
+    return res;
+};
 let list = L.range(4);
 
 log(list);
@@ -57,5 +66,33 @@ log(list);
 // log(list.next().value);
 log(reduce(add, list));
 
+//take 함수
+const take = curry((l, iter) =>{
+   let res = [];
+   for ( const a of iter) {
+       res.push(a);
+       if (res.length === l) return res;
 
+   }
+   return res;
+});
 
+const go = (...args) => reduce((a,f) => f(a), args);
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
+//log(take(5, range(100)));
+// console.time('');
+// log(take(5,L.range(100)));
+// console.timeEnd('');
+
+//go(range(1000),take(5),log);
+
+//L.map 이터레이터를 반환하는 제너레이터 함수
+
+L.map = function *(f,iter) {
+    for(const a of iter) yield f(a);
+};
+
+let it = L.map(a => a + 10, [1,2,3]);
+// log(it.next());
+// log(it.next());
+// log(it.next());
