d5b66aa5af671671e0ba15b732e357d4c34f046b
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2019-03-14 01:20:44 +0900

    20190314

--- 1st/7.js
+++ 1st/7.js
@@ -164,7 +164,7 @@ const queryStr = pipe(
 
 //8.4 take,find
 
-const users = [
+const users1 = [
     {age : 32},
     {age : 31},
     {age : 37},
@@ -202,7 +202,7 @@ const filter = curry(pipe(L.filter,takeAll));
 
 //8.6 L.flatten
 
-log([[1,2],3,4,...[5,6],...[7,8,9]]);
+// log([[1,2],3,4,...[5,6],...[7,8,9]]);
 
 const  isIterable = a => a && a[Symbol.iterator];
 
@@ -222,22 +222,22 @@ L.deepFlat = function *f(iter) {
     for (const a of iter) if (isIterable(a)) yield* f(a);
     else yield a;
 };
-log([...L.deepFlat([1, [2, [3, 4], [[5]]]])]);
+// log([...L.deepFlat([1, [2, [3, 4], [[5]]]])]);
 
 let it3 = L.flatten([[1,2],3,4,...[5,6],...[7,8,9]]);
-log([...it3]);
-
-log(it3.next());
-log(it3.next());
-log(it3.next());
-log(take(6, L.flatten([[1,2],3,4,...[5,6],...[7,8,9]])));
+// log([...it3]);
+//
+// log(it3.next());
+// log(it3.next());
+// log(it3.next());
+// log(take(6, L.flatten([[1,2],3,4,...[5,6],...[7,8,9]])));
 const flatten = pipe(L.flatten, takeAll);
 
-log(flatten([[1,2],3,4,...[5,6],...[7,8,9]]));
+// log(flatten([[1,2],3,4,...[5,6],...[7,8,9]]));
 
 // L.flatMap
 
-log([[1,2],[3,4],[5,6,7],8,9,[10]].flatMap(a=>a));
+// log([[1,2],[3,4],[5,6,7],8,9,[10]].flatMap(a=>a));
 
 //log([[1,2],[3,4],[5,6,7],8,9,[10]].map(a => a.map(a=> a * a)));
 
@@ -250,7 +250,7 @@ const flatMap = curry(pipe(L.map, flatten));
 
 let it4 = L.flatMap(a => a, [[1,2],[3,4],[5,6,7]]);
 
-log(flatMap(a=>a,[[1,2],[3,4],[5,6,7]]));
+//log(flatMap(a=>a,[[1,2],[3,4],[5,6,7]]));
 
 // log(it4.next());
 // log(it4.next());
@@ -260,6 +260,57 @@ log(flatMap(a=>a,[[1,2],[3,4],[5,6,7]]));
 // log(it4.next());
 // log(it4.next());
 
-log([...it4]);
+// log([...it4]);
+//
+// log(flatMap(range,map(a => a + 1, [1,2,3])));
 
-log(flatMap(range,map(a => a + 1, [1,2,3])));
+// const arr = [
+//     [1,2],
+//     [3,4,5],
+//     [6,7,8],
+//     [9,10]
+// ];
+//
+// go(
+//     arr,
+//     L.flatten,
+//     L.filter( a => a % 2),
+//     take(3),
+//     log
+// );
+
+let users = [
+    { name: 'a', age: 21, family: [
+    { name: 'a1', age: 53 },
+    { name: 'a2', age: 47 },
+    { name: 'a3', age: 16 },
+    { name: 'a4', age: 15 }
+        ] },
+    { name: 'b', age: 24, family: [
+    { name: 'b1', age: 58 },
+    { name: 'b2', age: 51 },
+    { name: 'b3', age: 19 },
+    { name: 'b4', age: 22 }
+        ] },
+    { name: 'c', age: 31, family: [
+    { name: 'c1', age: 64 },
+    { name: 'c2', age: 62 }]},
+    { name: 'd', age: 20, family:
+    [
+    { name: 'd1', age: 42 },
+    { name: 'd2', age: 42 },
+    { name: 'd3', age: 11 },
+    { name: 'd4', age: 7 }
+    ]}
+];
+
+go(
+    users,
+    L.map( u => u.family),
+    L.flatten,
+    L.filter( u => u.age < 20),
+    L.map( u => u.age),
+    take(3),
+    reduce(add),
+    log
+);
